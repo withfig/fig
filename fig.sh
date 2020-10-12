781a5b7a514484d79ff44a7620067083635cd9c9
Brendan Falk <brendan.falk97@gmail.com>
2020-10-12 15:05:49 -0700

    halfway through writing install script

--- fig.sh
+++ fig.sh
@@ -1,6 +1,21 @@
 # this is what will be added to everyone's .profile, .bash_profile, and .zprofile
 
 
-1. Source custom/figpath.sh (or should this just be saved in fig defaults??? probably...)
-2. Source aliases (source custom/aliases/*.sh)
-	Wildcard means you can download other people's aliases
+# 1. Source custom/figpath.sh (or should this just be saved in fig defaults??? probably...)
+# 2. Source aliases (source custom/aliases/*.sh)
+# 	Wildcard means you can download other people's aliases
+
+
+export FIGPATH="~/.fig/bin:~/run:"
+FIGPATH=$FIGPATH""
+
+##Run aliases shell script
+[ -s ~/.fig/custom/aliases/_myaliases.sh ] && source ~/.fig/custom/aliases/*.sh
+
+
+
+# Get current working directory of Terminal
+function cd() { builtin cd "$1"; fig bg:cd || true }
+fig bg:cd
+
+
