43960d37dab3a55b422369de087bbefa07ec9d7c
Brendan Falk <brendan@withfig.com>
2020-10-19 20:57:43 -0700

    fixed fig.sh

--- fig.sh
+++ fig.sh
@@ -7,8 +7,11 @@ then
 	# Backup for getting Fig's context
 	function cd() { if [ -n "$1" ]; then builtin cd "$1"; else builtin cd; fi; fig bg:cd || true; }
 
-	##Run aliases shell script
+	# Run aliases shell script
 	[ -s ~/.fig/user/aliases/_myaliases.sh ] && source ~/.fig/user/aliases/*.sh
+
+	# Check for prompts or onboarding
+	[ -s ~/.fig/tools/prompts.sh ] && source ~/.fig/tools/prompts.sh
 
 	FIG=1
 fi
